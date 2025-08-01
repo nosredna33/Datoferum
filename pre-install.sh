4523ee13a9d747b879bd74a9262be9488ec4e545
Nosredna333 <87626812+nosredna33@users.noreply.github.com>
2025-08-01 11:29:31 -0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -12,3 +12,19 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker ec2-user
 
+# Instala Hadoop 
+dnf install -y openjdk-11-jdk wget
+wget https://hadoop.apache.org/releases/hadoop-3.3.6.tar.gz
+tar -xzf hadoop-3.3.6.tar.gz -C /opt
+mv /opt/hadoop-3.3.6 /opt/hadoop
+
+# Instala Solr
+wget https://archive.apache.org/dist/solr/solr-9.3.0/solr-9.3.0.tgz
+tar -xzf solr-9.3.0.tgz -C /opt
+mv /opt/solr-9.3.0 /opt/solr
+
+# Instala Kafka
+wget https://kafka.apache.org/releases/3.7.0/kafka_2.13-3.7.0.tgz
+tar -xzf kafka_2.13-3.7.0.tgz -C /opt && \
+mv /opt/kafka_2.13-3.7.0 /opt/kafka
+
